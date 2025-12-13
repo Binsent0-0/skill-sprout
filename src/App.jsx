d0239604514c9f4badfc08e6ff010fe15583b3d3
Binsent0-0 <vincegheibrienne.rufino@email.lcup.edu.ph>
2025-12-13 16:08:59 +0800

    Home fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import AuthModal from './components/AuthModal';
 import ProtectedRoute from './components/ProtectedRoute';
 import ChatWidget from './components/ChatWidget';
 
-import Home from './pages/home';
+import Home from './pages/Home';
 import About from './pages/About';
 import Hobbies from './pages/Hobbies';
 import Tutors from './pages/Tutors';
